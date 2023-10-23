1dde66cfea15828e21324d689fa828503d039cf1
guideau jean <guideau221@unicaen.fr>
2023-10-23 17:50:12 +0200

    refactor: responsive mais reste quelque problème

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,11 @@
 function myFunction() {
     var x = document.getElementById("navbarNav");
-    if (x.className === "collapse navbar-collapse d-flex justify-content-end topnav") {
+    var y = document.getElementById("boutonBurguer")
+    if (x.className === "collapse navbar-collapse d-flex flex-row-reverse topnav") {
       x.className += " responsive";
+      y.className = "fa-solid fa-x fa-2xl";
     } else {
-      x.className = "collapse navbar-collapse d-flex justify-content-end topnav";
+      x.className = "collapse navbar-collapse d-flex flex-row-reverse topnav";
+      y.className = "fa-solid fa-bars fa-2xl";
     }
   }
